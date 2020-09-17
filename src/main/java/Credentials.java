bd856bf8f5704e76d702164716fe5d02dd860e85
bitliner <giovanni.gaglione@gmail.com>
2020-09-17 18:55:00 +0200

    added example of api-client for requesting a token

---
+++ src/main/java/Credentials.java
@@ -0,0 +1,20 @@
+
+public class Credentials {
+	String apiKey;
+	String secret;
+	public String getApiKey() {
+		return apiKey;
+	}
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+	public String getSecret() {
+		return secret;
+	}
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+	
+	
+
+}
---
+++ src/main/java/HttpPostGetToken.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+public class HttpPostGetToken {
+	public static void main(String[] args) throws ClientProtocolException, IOException {
+		String apiKey=args[0];
+		String secret=args[1];
+
+		System.out.println("apiKey: "+apiKey);
+		System.out.println("secret: "+secret);
+
+		String       postUrl       = "https://api.wonderflow.co/token";// put in your url
+		Gson         gson          = new Gson();
+		HttpClient   httpClient    = HttpClientBuilder.create().build();
+		HttpPost     post          = new HttpPost(postUrl);
+		Credentials credentials=new Credentials();
+		credentials.setApiKey(apiKey);
+		credentials.setSecret(secret);
+		StringEntity postingString = new StringEntity(gson.toJson(credentials));//gson.tojson() converts your pojo to json
+		post.setEntity(postingString);
+		post.setHeader("Content-type", "application/json");
+		HttpResponse  response = httpClient.execute(post);
+
+		if (response != null) {
+			InputStream in = response.getEntity().getContent(); //Get the data in the entity
+			Scanner s = new Scanner(in).useDelimiter("\\A");
+			String result = s.hasNext() ? s.next() : "";
+			JsonObject jsonResponse=new Gson().fromJson(result, JsonObject.class);
+			JsonElement token=jsonResponse.get("token");
+			System.out.println("Token is: "+token);
+
+			s.close();
+		}
+	}
+}
